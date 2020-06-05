eb61dab78331b7dd599b715c4ced6f746709c351
Marleen Johannsen <marleen.johannsen@g,x.de>
2020-06-05 16:00:09 +0200

    Generated Plates

--- Hausarbeit_BIVI.m
+++ Hausarbeit_BIVI.m
@@ -5,12 +5,47 @@ clear; %Variablen bereinigen
 close all; %alles schlieﬂen
 
 %% 1. PLATE GENERATOR
+script1_PlateGenerator;
+script2_TrafficImageGenerator;
+script3_TrafficResizeToSameSize;
+
+% Training Data
+% Load images from Plate Generator into data store for augmentation step 2
+%imageDS = imageDatastore('Folder?','IncludeSubfolders',true,'LabelSource','foldernames');  % create DataStore
+%[trainingImageDS,validationImageDS] = splitEachLabel(imageDS,0.7,'randomized'); %70% als Trainingsdatn, 30% als Valodation aufteilen
 
 %% 2. AUGMENTER
 
 %% 3. OWN AUGMENTER OPTIONAL
 
 %% 4. SET UP NETWORK
+
+% ----- einfaches DeepLearning Netzwerk definieren
+
+layers = [
+    imageInputLayer([28 28 1])
+    
+    convolution2dLayer(3,8,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    maxPooling2dLayer(2,'Stride',2)
+    
+    convolution2dLayer(3,16,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    maxPooling2dLayer(2,'Stride',2)
+    
+    convolution2dLayer(3,32,'Padding','same')
+    batchNormalizationLayer
+    reluLayer
+    
+    fullyConnectedLayer(10)
+    softmaxLayer
+    classificationLayer];
+
+
 
 %% 5. TRAIN NETWORK
 
--- script2_TrafficImageGenerator.m
+++ script2_TrafficImageGenerator.m
@@ -1,4 +1,3 @@
-
 %%%%%%%%%%%%%%%%%%%% Bildgenerator %%%%%%%%%%%
 % Fuegt Nummernschilder mit randomisierten Positionen und Groessen in
 % verschiedene Hintergrundbilder ein.
@@ -19,63 +18,56 @@ classcolors = {[0 0 0]    [255 255 255]};
 
 platesIds = imageDatastore('plates');
 backgroundIds = imageDatastore('backgrounds');
+for iPL=1:numel(platesIds.Files)
+    numBG = randi([1 numel(backgroundIds.Files)]);
+    bg_orig = readimage(backgroundIds, numBG);
+    bg = imresize(bg_orig, [360 480], 'bilinear'); % Inputgroesse des SegNets
+    [plate INFO] = readimage(platesIds, iPL);
+    [path name ext]=fileparts(INFO.Filename);
 
-for iBG=1:numel(backgroundIds.Files)
-    
-    bg_orig = readimage(backgroundIds,iBG);
-    bg_orig = imresize(bg_orig, [360 480], 'bilinear'); % Inputgroesse des SegNets
+    %% Ramdom Size
+    r = randi([40 300]) * 0.15 /100;
+    plate = imresize(plate, r, 'bilinear');
 
-    for iPL=1:numel(platesIds.Files)
-        bg = bg_orig;
-        [plate INFO] = readimage(platesIds, iPL);
-        [path name ext]=fileparts(INFO.Filename);
-    
-        %% Ramdom Size
-        r = randi([40 300]) * 0.15 /100;
-        plate = imresize(plate, r, 'bilinear');
+    % Konvertierung in uint8, da ansonsten beim Abspeichern der Classlabels im 
+    % Float-Format Rundungsfehler entstehen, wodurch die
+    % Classlabel-Farbwerte nicht exakt 255 oder 0 sind. Das SegNet
+    % funktioniert nur mit exakten Werten!
+    classlabel = uint8(zeros(size(bg)));
 
-        % Konvertierung in uint8, da ansonsten beim Abspeichern der Classlabels im 
-        % Float-Format Rundungsfehler entstehen, wodurch die
-        % Classlabel-Farbwerte nicht exakt 255 oder 0 sind. Das SegNet
-        % funktioniert nur mit exakten Werten!
-        classlabel = uint8(zeros(size(bg)));
-            
-        % Rondom position
-        m = floor(rand*(size(bg,1)-size(plate,1)));
-        n = floor(rand*(size(bg,2)-size(plate,2)));
+    % Rondom position
+    m = floor(rand*(size(bg,1)-size(plate,1)));
+    n = floor(rand*(size(bg,2)-size(plate,2)));
 
-        % Einfuegen
-        bg(1+m:m+size(plate,1), 1+n:n+size(plate,2), :) = plate;
-        
-        % BackgroundClassLabel
-        classlabel(:,:,1) = classcolors{1}(1);
-        classlabel(:,:,2) = classcolors{1}(2);
-        classlabel(:,:,3) = classcolors{1}(3);
-        
-        % PlateClassLabel
-        classlabel(1+m:m+size(plate,1), 1+n:n+size(plate,2), 1) = classcolors{2}(1);
-        classlabel(1+m:m+size(plate,1), 1+n:n+size(plate,2), 2) = classcolors{2}(2);
-        classlabel(1+m:m+size(plate,1), 1+n:n+size(plate,2), 3) = classcolors{2}(3);
-        
+    % Einfuegen
+    bg(1+m:m+size(plate,1), 1+n:n+size(plate,2), :) = plate;
+
+    % BackgroundClassLabel
+    classlabel(:,:,1) = classcolors{1}(1);
+    classlabel(:,:,2) = classcolors{1}(2);
+    classlabel(:,:,3) = classcolors{1}(3);
+
+    % PlateClassLabel
+    classlabel(1+m:m+size(plate,1), 1+n:n+size(plate,2), 1) = classcolors{2}(1);
+    classlabel(1+m:m+size(plate,1), 1+n:n+size(plate,2), 2) = classcolors{2}(2);
+    classlabel(1+m:m+size(plate,1), 1+n:n+size(plate,2), 3) = classcolors{2}(3);
+
 %         figure(1); imshow(imresize(bg,0.5))
 %         pause(1)
-        
-        folder = char("images/");
-        if ~exist(folder, 'dir')
-           mkdir(folder)
-        end
-        filename = char(name + "_" + num2str(iBG) + ".png");
-        fname=fullfile(folder,filename);
-        imwrite(bg,fname,'PNG');
 
-        folder = char("labels/");
-        if ~exist(folder, 'dir')
-           mkdir(folder)
-        end
-        filename = char(name + "_" + num2str(iBG) + ".png");
-        fname=fullfile(folder,filename);
-        imwrite(classlabel,fname,'PNG');
+    folder = char("images/");
+    if ~exist(folder, 'dir')
+       mkdir(folder)
     end
-        
+    filename = char(name + "_" + num2str(numBG) + ".png");
+    fname=fullfile(folder,filename);
+    imwrite(bg,fname,'PNG');
+
+    folder = char("labels/");
+    if ~exist(folder, 'dir')
+       mkdir(folder)
+    end
+    filename = char(name + "_" + num2str(numBG) + ".png");
+    fname=fullfile(folder,filename);
+    imwrite(classlabel,fname,'PNG');
 end
-    
