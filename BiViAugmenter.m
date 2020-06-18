34820fb1a33413cb935a0475c0ed411fa83800b3
Thore S <mcthore@gmail.com>
2020-06-18 21:49:21 +0200

    BiviAugmenter comments
    & deleted some images
    & edited .gitignore

--- BiViAugmenter.m
+++ BiViAugmenter.m
@@ -11,17 +11,17 @@ function BiViAugmenter(ds, p)
         for a=1:100
             f = file;
             shadow = randi([0 1]); %Schatten ja oder nein per Zufall
-            increase = randi([0 20]); 
-            color = randi([1 3]);
-            f(:,:,color) = f(:,:,color) + increase;
-            f(:,:,color) = min(f(:,:,color),255);
+            increase = randi([0 20]); % Zufallszahl für Farbwertaddition
+            color = randi([1 3]); % Zufalls-Farbindex 1, 2, 3   r, g, b
+            f(:,:,color) = f(:,:,color) + increase; % Zufallszahl auf Zufallsfarbwert addieren
+            f(:,:,color) = min(f(:,:,color),255); % auf 255 begrenzen, damit es ein valider RGB Wert bleibt
             %augmented1 = jitterColorHSV(file,'Hue',[0.05 1]);
-            augmented = jitterColorHSV(f,'Brightness',[-0.5 0]);
+            augmented = jitterColorHSV(f,'Brightness',[-0.5 0]); % Zufällige Helligkeit
                 if (shadow==1)
-                    augmented(1:floor(end/2),:,:) = jitterColorHSV(augmented(1:floor(end/2),:,:),'Brightness',[-0.3 0]);
+                    augmented(1:floor(end/2),:,:) = jitterColorHSV(augmented(1:floor(end/2),:,:),'Brightness',[-0.3 0]); % obere Bildhälfte wird verdunkelt
                 end
             [~, name, ~]=fileparts(info.Filename);
-            imwrite(augmented, fullfile(p, char(name + "_aug" + num2str(a) + ".png")));
+            imwrite(augmented, fullfile(p, char(name + "_aug" + num2str(a) + ".png"))); % Datei speichern
         end
     end
 end
